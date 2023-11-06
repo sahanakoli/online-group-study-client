f81bd8539fcdc099c6af863aa0b946168748bca1
Sahana Koli <sahanasultanakoli@gmail.com>
2023-11-06 18:32:05 +0600

    FAQ section added

--- src/Sheard/Faq.jsx
+++ src/Sheard/Faq.jsx
@@ -3,7 +3,7 @@
 const Faq = () => {
     return (
         <div className="mt-14">
-            <h2 className="text-3xl font-bold text-center ">frequently asked questions</h2>
+            <h2 className="text-4xl font-bold text-center ">frequently asked questions</h2>
            <div className="hero mt-8">
             <div className="hero-content flex-col lg:flex-row-reverse lg:gap-24">
               <img src="https://i.ibb.co/FBCcjvL/faq.png" className="max-w-sm rounded-lg shadow-2xl" />
@@ -13,22 +13,22 @@ const Faq = () => {
                 <div className="collapse  bg-[#F3F3F3] rounded-lg mb-8">
                   <div className="collapse-title text-xl font-medium">
                     <h2>1.How can I find or join an online study group?</h2>
-                    <p>You can join or create online study groups through social media, educational platforms, or your institutions learning management system.</p>
                   </div>
+                  <p className=" px-4 pb-3">You can join or create online study groups through social media, educational platforms, or your institutions learning management system.</p>
                 </div>
                  {/* second question  */}
                 <div className="collapse  bg-[#F3F3F3] rounded-lg mb-8">
                   <div className="collapse-title text-xl font-medium">
                     <h2>2.What are the benefits of online group study?</h2>
-                    <p>Online group study offers benefits such as peer support, shared knowledge, diverse perspectives, and motivation.</p>
                   </div>
+                  <p className=" px-4 pb-3">Online group study offers benefits such as peer support, shared knowledge, diverse perspectives, and motivation.</p>
                 </div>
                  {/* third question  */}
                 <div className="collapse  bg-[#F3F3F3] rounded-lg mb-8">
                   <div className="collapse-title text-xl font-medium">
                     <h2>3.What is the purpose of an assignment?</h2>
-                    <p>The purpose of a study assignment is to help you deepen your knowledge of a subject, develop critical thinking skills, and demonstrate your understanding.</p>
                   </div>
+                  <p className=" px-4 pb-3">The purpose of a study assignment is to help you deepen your knowledge of a subject, develop critical thinking skills, and demonstrate your understanding.</p>
                 </div>
               </div>
             </div>
