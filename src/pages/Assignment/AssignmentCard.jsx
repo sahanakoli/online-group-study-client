7a23074fd7e3bdc7c16fa3c9486c1e32ccdbe681
Sahana Koli <sahanasultanakoli@gmail.com>
2023-11-06 18:13:31 +0600

    navbar added

--- src/pages/Assignment/AssignmentCard.jsx
+++ src/pages/Assignment/AssignmentCard.jsx
@@ -7,14 +7,15 @@ const AssignmentCard = ({assignment}) => {
     const {_id,title,marks, date} = assignment;
     return (
         <div className="mt-8 w-9/12 mx-auto">
-            <div className="card w-96 h-[300px] bg-base-100 shadow-xl">
+            <div className="card w-11/12 mx-auto h-[300px] bg-base-100 shadow-xl">
                 <div className="card-body">
+                    {/* <p>sgdg</p> */}
                     <h2 className="card-title text-xl font-bold">Title: {title}</h2>
                     <p className="text-xl font-semibold">Marks: {marks}</p>
                     <p className="text-lg font-medium">Date: {date}</p>
-                    <div className="card-actions justify-end">
-                        <Link to={`/view/${_id}`}><button className="btn bg-blue-300">View </button></Link>
-                        <Link to={`/update/${_id}`}><button className="btn bg-blue-300">Update </button></Link>
+                    <div className="card-actions justify-center">
+                        <Link to={`/view/${_id}`}><button className="btn bg-[#B68C5A]">View </button></Link>
+                        <Link to={`/update/${_id}`}><button className="btn bg-[#B68C5A]">Update </button></Link>
                     </div>
                 </div>
             </div>
