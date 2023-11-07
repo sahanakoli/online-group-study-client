381122cb62f16986bb6fafe2c55b4e8eb10b4cad
Sahana Koli <sahanasultanakoli@gmail.com>
2023-11-07 23:16:28 +0600

    deleted an assignment

--- src/pages/MyAssignments/MyAssignments.jsx
+++ src/pages/MyAssignments/MyAssignments.jsx
@@ -1,29 +1,14 @@
-// import { useEffect, useState } from "react";
+
 import Navbar from "../../Sheard/Navbar";
-// import MyAssignmentCard from "./MyAssignmentCard";
+
 
 
 const MyAssignments = () => {
-    // const [createAssignment, setCreateAssignment] = useState([]);
-
-    // useEffect(() =>{
-    //     fetch('http://localhost:5000/createAssignment')
-    //     .then(res => res.json())
-    //     .then(data => setCreateAssignment(data))
-    // },[])
+    
     return (
         <div>
             <Navbar></Navbar>
-            {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
-            {
-                createAssignment.map(assignment =><MyAssignmentCard 
-                key={assignment.id} 
-                assignment={assignment}
-                createAssignment={createAssignment}
-                setCreateAssignment={setCreateAssignment}
-                ></MyAssignmentCard>)
-            }
-            </div> */}
+            
         </div>
     );
 };
